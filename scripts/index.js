9504eaea639c32bd3046fb8cd15d33d580646462
Olga <lelik_kokushkina@mail.ru>
2023-02-20 16:54:53 +0300

    Готовая проектная работа No. 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
 const editProfileButton = document.querySelector('.profile__button-edit');
-const PopUp = document.querySelector('.popup');
+const popUp = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__button-close');
-
-function openPopup () {
-  PopUp.classList.add('popup_opened');
-  }
-editProfileButton.addEventListener('click', openPopup);
-
-function closePopup () {
-  PopUp.classList.remove('popup_opened');
-  }
-closePopupButton.addEventListener('click', closePopup);
 
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-    const nameProfile = document.querySelector('.profile__name');
-    const jobProfile = document.querySelector('.profile__job');
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-}
-formElement.addEventListener('submit', handleFormSubmit); 
-formElement.addEventListener('submit', closePopup); 
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__job');
 
+function openPopup() {
+  popUp.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+editProfileButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popUp.classList.remove('popup_opened');
+}
+closePopupButton.addEventListener('click', closePopup);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closePopup();
+}
+formElement.addEventListener('submit', handleFormSubmit);
